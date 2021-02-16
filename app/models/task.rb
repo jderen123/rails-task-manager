c58f201c82d932ccfa2ec1f0bbd335a2a068f350
Jordan Deren <jordanderen1@gmail.com>
2021-02-16 20:30:20 +0000

    done

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,3 @@
 class Task < ApplicationRecord
-  validates :title, presence: true
-  validates :details, presence: true
-  validates :completed, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/new', to: 'tasks#new'
-  get '/tasks/:id', to: 'tasks#show'
-  post '/tasks', to: 'tasks#create'
+  # get '/tasks', to: 'tasks#index'
+  #   patch '/tasks/:id', to: 'tasks#update'
+  # post '/tasks', to: 'tasks#create'
+  # get '/tasks/new', to: 'tasks#new'
+  #  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # get '/tasks/:id', to: 'tasks#show', as: :show
+  resources :tasks
+
+
+
 end
